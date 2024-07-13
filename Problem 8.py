37b3b625128e1c0d906d62a96f3abf2eecbd1d3e
ThivyashreeNS <nstshree2000@gmail.com>
2024-07-13 11:17:14 +0530

    Add files via upload

---
+++ Problem 8.py
@@ -0,0 +1,11 @@
+""" Problem 8: Write a a python program to find the minimum element in a rated and sorted list."""
+            
+# given list
+list = [4,3,6,5,1,2]
+list.sort()
+print("The sorted list:",list)
+
+# To find the minimum element
+min_element = list[0]
+
+print("The minimum element in the list is:",min_element)
